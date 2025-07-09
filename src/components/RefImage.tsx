520b1ad83f8e74e207f3a8ef8d06868918542e0f
emmynando <alexander.emmyxiano@gmail.com>
2025-07-09 13:54:39 +0100

    skeleton

--- src/components/RefImage.tsx
+++ src/components/RefImage.tsx
@@ -2,17 +2,15 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { useEffect, useRef, useState } from "react";
+import { TextPlugin } from "gsap/all";
+import { useRef } from "react";
 
 const CAMERAROLL = [
   { id: "c1", image: "/images/cam-roll1.avif", imageHeight: "60%" },
   { id: "c2", image: "/images/can-roll2.avif", imageHeight: "30%" },
-  // { id: "c3", image: "/images/cam-roll3.jpg", imageHeight: "80%" },
-  // { id: "c4", image: "/images/cam-roll4.avif", imageHeight: "80%" },
-  // { id: "c5", image: "/images/cma-roll5.avif", imageHeight: "80%" },
 ];
 
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, TextPlugin);
 type RefsType = {
   [key: string]: HTMLDivElement | null;
 };
@@ -20,38 +18,45 @@ type RefsType = {
 export default function RefenceImage() {
   const rollDivRef = useRef<HTMLDivElement | null>(null);
   const refs = useRef<RefsType>({});
-  const firstImgRef = useRef<number>(0);
-
-  const triggerRef = useRef(null);
 
   useGSAP(
     () => {
-      // Single ScrollTrigger that handles everything
-      // const mainTl = gsap.timeline();
-      const imgRollDiv = document.getElementById("imgRollDiv");
-
-      // const computedStyle = getComputedStyle(imgRollDiv);
       gsap.set("#imgRollDiv", {
         width: "50%",
         y: 100,
       });
+      gsap.set("#firstText", { text: "" });
       const mainTl = gsap.timeline({
         scrollTrigger: {
           trigger: rollDivRef.current,
           start: "top 90%",
-          end: "top 30%",
+          end: "top 70%",
           scrub: 1,
           markers: true,
         },
       });
       if (!rollDivRef.current) return;
-      // gsap.set("#imgRollDiv",);
       mainTl.to("#imgRollDiv", {
         width: "30%",
         y: 0,
         duration: 1,
         ease: "power2.out",
       });
+      mainTl.to(
+        "#firstText",
+        {
+          text: {
+            rtl: false,
+            // speed: ,
+            value:
+              "The aF-1 helps you to slow down, observe your surroundings and focus on the beauty of the present",
+            delimiter: "",
+          },
+          duration: 1,
+          ease: "power2.inOut",
+        },
+        "<"
+      );
     },
     {
       dependencies: [],
@@ -76,27 +81,13 @@ export default function RefenceImage() {
         </h2>
       </div>
 
-      {/* <span id="secondText" className="absolute bottom-[10%] right-[5%] z-10">
-        <h3 className=" w-[10ch] md:w-[19ch] text-white leading-[1] text-base md:text-[28px] -tracking-[1.5px] font-bold">
-          Discover the joy of capturing life, one frame at a time
-        </h3>
-      </span>
-      <span id="thirdText" className="absolute bottom-[10%] right-[5%] z-2">
-        <h3 className="w-[8ch]  text-white leading-[1] text-base md:text-[28px] -tracking-[1.5px] font-bold ">
-          Greatness, takes time
-        </h3>
-      </span>
-      {/* <div
-        className="flex justify-center items-center w-full bg-[#080808] overflow-y-scroll no-scrollbar"
-        id="rollDiv"
-      > */}
       <div
         className="flex justify-center items-center w-full bg-[#080808] no-scrollbar overflow-y-hidden h-dvh"
         id="rollDiv"
         ref={rollDivRef}
       >
         <h2
-          className="text-white text-center font-garamond font-normal w-[90%] md:w-[90%] leading-[1] -tracking-[1.5px] text-[28px] md:text-[64px] absolute top-[45%] md:top-[130%] left-[5%] md:left-[5%] z-10"
+          className="text-white text-center font-garamond font-normal w-[90%] md:w-[90%] leading-[1] -tracking-[1.5px] text-[28px] md:text-[64px] absolute top-[45%] md:top-[150%] left-[5%] md:left-[5%] z-10"
           id="firstText"
         >
           The aF-1 helps you to slow down, observe your surroundings and focus
@@ -123,81 +114,6 @@ export default function RefenceImage() {
           ))}
         </div>
       </div>
-      {/* next div */}
     </main>
   );
 }
-
-// useEffect(() => {
-//     const el = refs.current["c1"];
-//     if (!el) return;
-
-//     let prevY = el.getBoundingClientRect().y;
-//     let rafId: number;
-
-//     const track = () => {
-//       const y = el.getBoundingClientRect().y;
-//       if (y !== prevY) {
-//         firstImgRef.current = y;
-//         // console.log("Y changed:", y);
-//         if (firstImgRef.current < 613) {
-//           setFirstImg(true);
-//         }
-//         prevY = y;
-//       }
-//       rafId = requestAnimationFrame(track);
-//     };
-
-//     track();
-
-//     return () => cancelAnimationFrame(rafId);
-//   }, []);
-
-// useEffect(() => {
-//   const el = refs.current["c1"];
-//   if (!el) return;
-
-//   const observer = new IntersectionObserver(([entry]) => {
-//     if (entry.isIntersecting) {
-//       const rect = el.getBoundingClientRect();
-//       console.log(rect);
-//     }
-//   });
-
-//   observer.observe(el);
-
-//   return () => observer.disconnect();
-// }, [refs.current["c1"].y]);
-
-// ScrollTrigger.create({
-//               trigger: "#c1",
-//               start: "top top",
-//               end: "bottom 80%",
-//               pin: true,
-//               pinSpacing: false,
-//               markers: true, // Remove this in production
-//               onEnter: () => console.log("c1 enter and pinned"),
-//               onLeave: () => console.log("c1 leave and unpinned"),
-//               onEnterBack: () => console.log("c1 enter back"),
-//               onLeaveBack: () => console.log("c1 leave back"),
-//             });
-
-// ScrollTrigger.create({
-//       trigger: "#mainContainer",
-//       pin: true,
-//       pinSpacing: false,
-//       start: "top top",
-//       end: "+=200%", // Adjust based on your content
-//       onUpdate: (self) => {
-//         // Animate elements based on scroll progress
-//         const progress = self.progress;
-
-//         // Example animations based on scroll progress
-//         gsap.to("#imageRoll", {
-//           display: "block",
-//           y: progress * -50,
-//           duration: 0.1,
-//           ease: "elastic.in",
-//         });
-//       },
-//     });
